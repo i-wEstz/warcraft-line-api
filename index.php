393b762baa7bbd5eb00eea03229a55f5b83d982d
Kritchapon Choktananan <iwestz120@gmail.com>
2017-03-07 18:39:28 +0700

    add mongo connection

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
-echo "Hello LINE BOT";
+try
+{
+    $connection = new Mongo('mongodb://iwestz:iwestz@ds145389.mlab.com:45389/wow-api-ah');
+    $database   = $connection->selectDB('wow-api-ah');
+    $collection = $database->selectCollection('AH');
+}
+catch(MongoConnectionException $e)
+{
+    die("Failed to connect to database ".$e->getMessage());
+}
+
+$cursor = $collection->find();
+
+?>
