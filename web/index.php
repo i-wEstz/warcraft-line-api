cd92383a79de671ac1fbc5dcc66defb65d293b9b
Kritchapon Choktananan <iwestz120@gmail.com>
2017-03-07 18:43:50 +0700

    update mongo connection

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-    $connection = new Mongo('mongodb://iwestz:iwestz@ds145389.mlab.com:45389/wow-api-ah');
+    $connection = new Mongo('mongodb://iwestz:lakious1@ds145389.mlab.com:45389/wow-api-ah');
     $database   = $connection->selectDB('wow-api-ah');
     $collection = $database->selectCollection('AH');
 }
