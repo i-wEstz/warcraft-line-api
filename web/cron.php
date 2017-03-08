03d9015d62028f1438a22a929be61ae620ad31a0
Kritchapon Choktananan <iwestz120@gmail.com>
2017-03-08 19:54:33 +0700

    update table

--- web/cron.php
+++ web/cron.php
@@ -1,10 +1,11 @@
 <?php
 require('connection.php');
-ini_set('max_execution_time', 300);
+ini_set('max_execution_time', 1000);
 $collection = $database->selectCollection('AH');
 $collection_item = $database->selectCollection('item_list');
 
 $list = array();
+$name = array();
 $str = file_get_contents('https://us.api.battle.net/wow/auction/data/dreadmaul?locale=en_US&apikey=4vz7v2gtujvqtkprvy85f6mj9fwaanb8');
 $json = json_decode($str, true); // decode the JSON into an associative array
 $data_url = $json['files'][0]['url'];
@@ -25,10 +26,18 @@ print_r('Successfully Collect Data with '.count($ah_data).' Records');
 
 $item_list = array_unique($list);
 sort($item_list);
+// $collection_item->insert($item_list);
 
-foreach ($item_list as $val) {
-$collection_item->insert($val);
+foreach($item_list as $val){
+
+$item_name = file_get_contents('https://us.api.battle.net/wow/item/'.$val.'?locale=en_US&apikey=4vz7v2gtujvqtkprvy85f6mj9fwaanb8');
+$json_item = json_decode($item_name, true); // decode the JSON into an associative array
+$name[$json_item['name']] = $val;
+
 }
+
+print "<br>";
+print_r('Data: '.$name.'');
 
 print "<br>";
 print_r('Successfully insert unique with '.count($item_list).' Records');
