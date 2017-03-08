47b169c49d88502e31cf66dede16cafa53371672
Kritchapon Choktananan <iwestz120@gmail.com>
2017-03-08 17:21:15 +0700

    update

--- web/cron.php
+++ web/cron.php
@@ -1,35 +1,46 @@
 <?php
 require('connection.php');
 $date_db = $database->selectCollection('flag');
+$itemL = $database->selectCollection('item_list');
 ini_set('max_execution_time', 300);
-
+$list = array();
 $str = file_get_contents('https://us.api.battle.net/wow/auction/data/dreadmaul?locale=en_US&apikey=4vz7v2gtujvqtkprvy85f6mj9fwaanb8');
 $json = json_decode($str, true); // decode the JSON into an associative array
 $data_url = $json['files'][0]['url'];
-$lastModi = $json['files'][0]['lastModified'];
+$lastModi['lastModified'] = $json['files'][0]['lastModified'];
 
 $json_data = json_decode(file_get_contents($data_url),true);
-$lastModi = json_encode($lastModi);
+$lastModi = json_encode($lastModi['lastModified']);
 $auction_data = $json_data['auctions'];
 
+$timeQuery = array('lastModified' => $lastModi['lastModified']);
 
+$cursor_2 = $date_db->find($timeQuery);
 
-// print "<pre>";
-// print_r($details);
-// print "</pre>";
+if(!isset($cursor_2)){
+$date_db->insert($lastModi);
 $collection->remove(array(),array('safe' => true));
 foreach ($json_data['auctions'] as $key=>$value) {
     // print_r($value['item']);
     // $item_no = $value['item']; 
     // $item_data = json_decode(file_get_contents('https://us.api.battle.net/wow/item/'.$item_no.'?locale=en_US&apikey=4vz7v2gtujvqtkprvy85f6mj9fwaanb8'),true);
     // $item_name = $item_data['name'];
+    array_push($list,$value['item']);
     $collection->insert($value);
+    // $item_list = unique_multidim_array($auction_data,'item');
+
+
 }
+$itemL->remove(array(),array('safe' => true));
+$itemL->insert($list);
+}
+else{
+
+}
+
 // print "<pre>";
 // print_r($json_data['auctions']);
 // print "</pre>";
-
-$item_list = unique_multidim_array($auction_data,'item');
 
 function unique_multidim_array($array, $key) { 
     $temp_array = array(); 
@@ -45,4 +56,5 @@ function unique_multidim_array($array, $key) {
     } 
     return $temp_array; 
 } 
+
 ?>
