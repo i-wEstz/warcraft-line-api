940e1ab710e4833565f47f167505e4e6af980851
Kritchapon Choktananan <iwestz120@gmail.com>
2017-03-08 18:47:34 +0700

    edit mongo

--- web/connection.php
+++ web/connection.php
@@ -1,8 +1,10 @@
 <?php
 try
 {
-    $connection = new Mongo('mongodb://iwestz:lakious1@ds145389.mlab.com:45389/wow-api-ah');
+    $connection = new MongoClient('mongodb://iwestz:lakious1@ds145389.mlab.com:45389/');
     $database   = $connection->selectDB('wow-api-ah');
+    // $db_name = 'wow-api-ah';
+    // $database =  $connection->wow-api-ah;
     // $collection = $database->selectCollection('AH');
 }
 catch(MongoConnectionException $e)
