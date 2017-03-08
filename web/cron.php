8ae43979701436649f0549e073bfc89362e98bdb
Kritchapon Choktananan <iwestz120@gmail.com>
2017-03-08 19:27:47 +0700

    update 121

--- web/cron.php
+++ web/cron.php
@@ -3,21 +3,30 @@ require('connection.php');
 ini_set('max_execution_time', 300);
 $collection = $database->selectCollection('AH');
 
-
+$list = array();
 $str = file_get_contents('https://us.api.battle.net/wow/auction/data/dreadmaul?locale=en_US&apikey=4vz7v2gtujvqtkprvy85f6mj9fwaanb8');
 $json = json_decode($str, true); // decode the JSON into an associative array
 $data_url = $json['files'][0]['url'];
 
 $json_data = json_decode(file_get_contents($data_url),true);
+$ah_data = $json_data['auctions'];
 
 $collection->remove(array(),array('w' => true));
-foreach ($json_data['auctions'] as $key=>$value) {
+foreach ($ah_data as $key=>$value) {
 
 
     $collection->insert($value);
-
+    array_push($list,$value['item']);
 
 
 }
-print_r('Successfully Collect Data');
+print_r('Successfully Collect Data with '.count($ah_data).' Records');
+
+$item_list = array_unique($list);
+sort($item_list);
+
+print_f($item_list);
+
+
+
 ?>
