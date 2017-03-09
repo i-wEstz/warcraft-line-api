58501d06455a8273f7163210b0a1566f92a7baa6
Kritchapon Choktananan <iwestz120@gmail.com>
2017-03-09 13:39:44 +0700

    update

--- web/bot.php
+++ web/bot.php
@@ -2,5 +2,8 @@
 require('connection.php');
 $collection_item = $database->selectCollection('item_list');
 $exist = $collection_item->find(array('item' => 720))->limit(1);
-print_r(iterator_to_array($exist));
+foreach ($exist as $doc) {
+print_r($doc['name'].$doc['item']);
+}
+// print_r(iterator_to_array($exist));
 ?>
