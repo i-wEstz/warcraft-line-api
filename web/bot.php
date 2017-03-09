25e3cdb5739c393b5247811754304848430e65d5
Kritchapon Choktananan <iwestz120@gmail.com>
2017-03-09 13:36:13 +0700

    update

--- web/bot.php
+++ web/bot.php
@@ -1,6 +1,6 @@
 <?php
 require('connection.php');
 $collection_item = $database->selectCollection('item_list');
-$exist = $collection_item->find(array('item' => 720),array('limit' => 1));
+$exist = $collection_item->find(array('item' => 720))->limit(1);
 print_r(iterator_to_array($exist));
 ?>
