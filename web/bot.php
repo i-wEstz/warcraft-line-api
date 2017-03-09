141442d30f39fd5e8326dff882ed11f7631b76c6
Kritchapon Choktananan <iwestz120@gmail.com>
2017-03-09 16:33:44 +0700

    test bot

--- web/bot.php
+++ web/bot.php
@@ -19,9 +19,6 @@
 require_once('LineBotTiny.php');
 require('connection.php');
 
-$collection = $database->selectCollection('AH');
-$collection_item = $database->selectCollection('item_list');
-
 $channelAccessToken = 'INlnJAtPlk6fbtEdrKwJs2Qvb9g4sRN7CWhm9GNWVbjYalLUXPf4rrqu6yVb+Chs1kePBrggCJ5NQgRGRSx/cnSg6E7ZLnU0Rj/Uf8C2cCWqFSaJDQbnfjffjW2R2iohgepVVIbgnRYm113ZEGTJOQdB04t89/1O/w1cDnyilFU=';
 $channelSecret = 'b600e84645566513de2c79423dcfa139';
 
@@ -59,6 +56,9 @@ foreach ($client->parseEvents() as $event) {
 
 function Message($message_in){
 
+    $collection = $database->selectCollection('AH');
+    $collection_item = $database->selectCollection('item_list');
+    
     $str = trim(substr($message_in,2,strlen($message_in)));
     $cursor = $collection_item->findOne(array('name' => 'Bent Staff'));
     return 'kuy id is '.$cursor['item'];
