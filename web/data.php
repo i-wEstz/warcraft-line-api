ff611b257ed721819876b6e263a2abde2721344e
Kritchapon Choktananan <iwestz120@gmail.com>
2017-03-09 16:46:14 +0700

    test bot

--- web/data.php
+++ web/data.php
@@ -2,7 +2,7 @@
 require('connection.php');
 $collection_item = $database->selectCollection('item_list');
 $collection = $database->selectCollection('AH');
-$cursor = $collection_item->findOne(array('name' => 'Bent Staff'));
+$cursor = $collection_item->findOne(array('name' => 'Stormray'));
 $ah = $collection->find(array('item' => $cursor['item']))->sort(array('buyout' => 1))->limit(1);
 
 print_r($ah);
