7aaa3784f51ab63ca0817e5b74d9724d7e8269a9
Kritchapon Choktananan <iwestz120@gmail.com>
2017-03-10 17:23:39 +0700

    add regex

--- web/bot.php
+++ web/bot.php
@@ -89,7 +89,9 @@ function Message($message_in,$collection,$collection_item){
         $bronze = substr($data['buyout'],-2);
         $silver = substr($data['buyout'],-4,2);
         $gold = substr($data['buyout'],0,($length-4));
-        $text_1 = "< ".$cursor['name']." >"."\n--------------\n"."1.) ราคาถูกที่สุด\nBuyout: ".$gold.'g'.$silver.'s'.$bronze."b\n".'จำนวน: '.$data['quantity']."\nตั้งโดย: ".$data['owner']."\n========\n";
+        // $text_1 = "< ".$cursor['name']." >"."\n--------------\n"."1.) ราคาถูกที่สุด\nBuyout: ".$gold.'g'.$silver.'s'.$bronze."b\n".'จำนวน: '.$data['quantity']."\nตั้งโดย: ".$data['owner']."\n========\n";
+        $text_1 = "< ".$cursor['name']." >"."\n--------------\n"."1.) ราคาถูกที่สุด\nBuyout: ".$gold.'.'.$silver."g\n".'จำนวน: '.$data['quantity']."\nตั้งโดย: ".$data['owner']."\n========\n";
+
     } 
         $ah = $collection->find(array('item' => $cursor['item'],'buyout' => array('$gt' => 0)))->sort(array('quantity' => -1,'buyout'=> 1))->limit(1);
 
@@ -103,7 +105,7 @@ function Message($message_in,$collection,$collection_item){
         $br = substr($data['viable'],-2);
         $si = substr($data['viable'],-4,2);
         $go = substr($data['viable'],0,($len-4));
-        $text_2 = "2.) ถูกที่สุดและจำนวนมากสุด"."\nBuyout: ".$gold.'g'.$silver.'s'.$bronze."b\n".'จำนวน: '.$data['quantity']."\nตั้งโดย: ".$data['owner']."\nราคาต่อชิ้น: ".$go."g".$si."s".$br."b\n========\n";
+        $text_2 = "2.) ถูกที่สุดในปริมาณมากที่สุด"."\nBuyout: ".$gold.'.'.$silver."g\n".'จำนวน: '.$data['quantity']."\nตั้งโดย: ".$data['owner']."\nราคาต่อชิ้น: ".$go.".".$si."g\n========\n";
     } 
     $ah = $collection->find(array('item' => $cursor['item'],'buyout' => array('$gt' => 0)))->sort(array('viable' => 1))->limit(1);
 
@@ -117,7 +119,7 @@ function Message($message_in,$collection,$collection_item){
         $br = substr($data['viable'],-2);
         $si = substr($data['viable'],-4,2);
         $go = substr($data['viable'],0,($len-4));
-        $text_3 = "3.) คุ้มค่าที่สุด(ต่อชิ้น)"."\nBuyout: ".$gold.'g'.$silver.'s'.$bronze."b\n".'จำนวน: '.$data['quantity']."\nตั้งโดย: ".$data['owner']."\nราคาต่อชิ้น: ".$go."g".$si."s".$br."b\n========\n";
+        $text_3 = "3.) คุ้มค่าที่สุด(ต่อชิ้น)"."\nBuyout: ".$gold.'.'.$silver."g\n".'จำนวน: '.$data['quantity']."\nตั้งโดย: ".$data['owner']."\nราคาต่อชิ้น: ".$go.".".$si."g\n========\n";
     } 
     $text_result = $text_1.$text_2.$text_3; 
     }
